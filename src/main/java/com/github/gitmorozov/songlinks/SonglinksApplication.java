5bd87c16dfbf7bcbe5f5c116dbe98932db356d5d
Andrey  Morozov <endefr@yandex.ru>
2023-02-24 19:54:37 +0300

    initial commit

---
+++ src/main/java/com/github/gitmorozov/songlinks/SonglinksApplication.java
@@ -0,0 +1,13 @@
+package com.github.gitmorozov.songlinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonglinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonglinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gitmorozov/songlinks/SonglinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gitmorozov.songlinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonglinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
